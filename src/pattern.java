edb53a8bb75ec875ee14a519c850ecaa5657948e
manis <manprajapati43@gmail.com>
2024-10-24 14:47:27 -0500

    pattern1

--- src/pattern.java
+++ src/pattern.java
@@ -1,15 +1,5 @@
 public class pattern {
     public static void main(String[] args) {
-//        int[] arr1= new int[5];
-//        for(int i=0;i<5;i++){
-//
-//            for(int j=0;j<=i;j++){
-//
-//                System.out.print("*");
-//            }
-//            System.out.println("\n");
-//        }
-//
         int i, j;
         for (i = 1; i <= 6; i++) {
             for (j = 1; j <= 6 - i; j++) {
@@ -22,3 +12,9 @@ public class pattern {
         }
     }
 }
+//     *
+//    * *
+//   * * *
+//  * * * *
+// * * * * *
+//* * * * * *
